1bdf3c6fa5d5b9f47ceed2dac3fa5f0d2bd784f7
nesladka <147166345+nesladka@users.noreply.github.com>
2023-10-06 20:12:17 +0300

    Create solution2.py

---
+++ задача 2/solution2.py
@@ -0,0 +1,37 @@
+import pulp
+import time
+
+start = time.time()
+
+x = pulp.LpVariable("x", lowBound=0)
+y = pulp.LpVariable("y", lowBound=0)
+
+problem_max = pulp.LpProblem('0_max', pulp.LpMaximize)
+problem_max += x - 2 * y, "Objective Function"
+problem_max += 5 * x + 3 * y >= 30, "1"
+problem_max += x - y <= 3, "2"
+problem_max += -3 * x + 5 * y <= 15, "3"
+problem_max.solve()
+
+for variable in problem_max.variables():
+    print(variable.name, "=", variable.varValue)
+
+print("Max:")
+print(pulp.value(problem_max.objective))
+
+problem_min = pulp.LpProblem('0_min', pulp.LpMinimize)
+problem_min += x - 2 * y, "Objective Function"
+problem_min += 5 * x + 3 * y >= 30, "1"
+problem_min += x - y <= 3, "2"
+problem_min += -3 * x + 5 * y <= 15, "3"
+problem_min.solve()
+
+for variable in problem_min.variables():
+    print(variable.name, "=", variable.varValue)
+
+print("Min:")
+print(pulp.value(problem_min.objective))
+
+stop = time.time()
+print("Time:")
+print(stop - start)
